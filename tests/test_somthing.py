ea700f69f333f7703db9c7f693636dad01847487
denis <92470430+Mousport@users.noreply.github.com>
2024-02-18 17:50:54 +0300

    Add fixture

--- tests/test_somthing.py
+++ tests/test_somthing.py
@@ -1,5 +1,4 @@
-import requests
-from configuration import SERVICE_URL
+
 from src.baseclasses.response import Response
 from src.schemas.user import User
 
@@ -8,9 +7,7 @@ from src.schemas.user import User
 # res = requests.get(SERVICE_URL)
 # # print(res.json())
 
-def test_getting_users_list():
-    response = requests.get(SERVICE_URL)
-    test_object = Response(response)
-    test_object.assert_status_code(200).validate(User)
+def test_getting_users_list(get_user):
+    Response(get_user).assert_status_code(200).validate(User)
 
 
