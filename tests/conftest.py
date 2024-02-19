ff5856410232820a1372d4579b1e000f39b3e08a
denis <92470430+Mousport@users.noreply.github.com>
2024-02-19 11:32:53 +0300

    Добавление маркировок и фикстур

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,3 +5,20 @@ from random import randrange
 @pytest.fixture
 def get_number():
     return randrange(1, 1000, 5)
+
+
+@pytest.fixture
+def make_number():
+    # print('I get number')
+    numb = randrange(1, 100, 2)
+    yield numb
+    # print(f'Number is {numb}')
+
+
+def _calculate(a, b):
+    return a + b
+
+
+@pytest.fixture
+def calculate():
+    return _calculate
--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 
 from src.baseclasses.response import Response
 from src.schemas.user import User
@@ -7,8 +8,15 @@ from src.schemas.user import User
 # res = requests.get(SERVICE_URL)
 # # print(res.json())
 
-def test_getting_users_list(get_user, get_number):
+def test_getting_users_list(get_user, get_number, make_number):
     Response(get_user).assert_status_code(200).validate(User)
-    print(get_number)
+    # print(make_number)
 
-
+@pytest.mark.developmet
+@pytest.mark.parametrize('first_value, second_value, result', [
+    (1, 2, 3),
+    (-1, -2, -3),
+    (-1, 2, 1)
+])
+def test_calculate(first_value, second_value, result, calculate):
+    assert calculate(first_value, second_value) == result
